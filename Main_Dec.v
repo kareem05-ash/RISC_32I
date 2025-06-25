76c9306e10ee4855e785e76e07a7c231b937e3b3
kareem05-ash <kareem.ash05@gmail.com>
2025-06-25 03:37:33 +0300

    Add files via upload

--- Main_Dec.v
+++ Main_Dec.v
@@ -1,19 +1,70 @@
 module main_dec
 (
-    input [6:0] Opcode,
-    output reg RegWrite,ALUsrc,MemWrite,ResultSrc,Branch,
-    output reg [1:0]Immsrc,ALUOp
+    input [6:0] opcode ,
+    output reg [1:0] ImmSrc , ALUOP ,
+    output reg RegWrite , ALUSrc , MemWrite , ResultSrc , Branch
 );
-
-always@(*)
-case(Opcode)
-        7'b000_0011: begin RegWrite=1; Immsrc=2'b00; ALUsrc=1; MemWrite=0; ResultSrc=1; Branch=0; ALUOp=2'b00; end 
-        7'b010_0011: begin RegWrite=0; Immsrc=2'b01; ALUsrc=1; MemWrite=1; ResultSrc='x; Branch=0; ALUOp=2'b00; end 
-        7'b011_0011: begin RegWrite=1; Immsrc=2'bxx; ALUsrc=0; MemWrite=0; ResultSrc=0; Branch=0;ALUOp=2'b10; end 
-        7'b001_0011: begin RegWrite=1; Immsrc=2'b00; ALUsrc=1; MemWrite=0; ResultSrc=0; Branch=0; ALUOp=2'b10; end 
-        7'b110_0011: begin RegWrite=0; Immsrc=2'b10; ALUsrc=0; MemWrite=0; ResultSrc='x; Branch=1; ALUOp=2'b01; end 
-        default:     begin RegWrite=0; Immsrc=2'b00; ALUsrc=0; MemWrite=0; ResultSrc=0; Branch=0; ALUOp=2'b00; end 
-
-endcase
-
+    always@(*)
+        case(opcode)
+            7'b000_0011 : 
+            begin
+                ImmSrc = 2'b00;
+                ALUOP = 2'b00;
+                RegWrite = 1;
+                ALUSrc = 1;
+                MemWrite = 0;
+                ResultSrc = 1;
+                Branch = 0;
+            end
+            7'b010_0011 :
+            begin
+                ImmSrc = 2'b01;
+                ALUOP = 2'b00;
+                RegWrite = 0;
+                ALUSrc = 1;
+                MemWrite = 1;
+                ResultSrc = 1'bx;
+                Branch = 0;
+            end
+            7'b011_0011 : 
+            begin
+                ImmSrc = 2'bxx;
+                ALUOP = 2'b10;
+                RegWrite = 1;
+                ALUSrc = 0;
+                MemWrite = 0;
+                ResultSrc = 0;
+                Branch = 0;
+            end
+            7'b001_0011 :
+            begin
+                ImmSrc = 2'b00;
+                ALUOP = 2'b10;
+                RegWrite = 1;
+                ALUSrc = 1;
+                MemWrite = 0; 
+                ResultSrc = 0; 
+                Branch = 0;
+            end
+            7'b110_0011 : 
+            begin
+                ImmSrc = 2'b10;
+                ALUOP = 2'b01;
+                RegWrite = 0;
+                ALUSrc = 0;
+                MemWrite = 0;
+                ResultSrc = 1'bx;
+                Branch = 1;
+            end
+            default : 
+            begin
+                ImmSrc = 0;
+                ALUOP = 0;
+                RegWrite = 0; 
+                ALUSrc = 0;
+                MemWrite = 0;
+                ResultSrc = 0;
+                Branch = 0;
+            end
+    endcase
 endmodule
