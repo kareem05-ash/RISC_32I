5c1e6258c8c2f9ac4fa18ae9b76e2dfea852479a
kareem05-ash <kareem.ash05@gmail.com>
2025-06-28 20:35:58 +0300

    Delete Next_PC_Logic.v

--- Next_PC_Logic.v
+++
@@ -1,9 +0,0 @@
-module next_pc_logic
-(
-    input [31:0] PC , 
-    input [31:0] ImmExt , 
-    input PCSrc , 
-    output [31:0] NextPC
-);
-    assign NextPC = PCSrc? PC+ImmExt : PC+4;
-endmodule
