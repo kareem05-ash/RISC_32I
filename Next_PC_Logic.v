e223208cf99f9b96e3ae16be57d524c8b298e6fc
Kareem Ashraf <ashrafkareem825@gmail.com>
2025-06-30 16:22:21 +0300

    all deleted

--- Next_PC_Logic.v
+++
@@ -1,9 +0,0 @@
-module next_pc_logic
-(
-    input [31:0] PC ,                           
-    input [31:0] ImmExt , 
-    input PCSrc , 
-    output [31:0] PCNext
-);
-    assign PCNext = PCSrc? PC+ImmExt : PC+4;
-endmodule
